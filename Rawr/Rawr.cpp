e077f81a5873c2f021d722ffc3e052c0d3a2e036
Sofia-pastrami <nore0404@gmail.com>
2021-01-17 02:10:06 +0100

    Hotfixes and added menu items.

--- Rawr/Rawr.cpp
+++ Rawr/Rawr.cpp
@@ -1,5 +1,4 @@
 // Rawr.cpp : Defines the entry point for the application.
-//
 
 #include "framework.h"
 #include "Rawr.h"
--- Rawr/Rawr.h
+++ Rawr/Rawr.h
@@ -1,3 +1,5 @@
+// Include resource
+
 #pragma once
 
 #include "resource.h"
